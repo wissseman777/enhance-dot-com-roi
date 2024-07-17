22dde406eda3fb24efaf6d78706c8b446995b687
wissseman777 <161206102+wissseman777@users.noreply.github.com>
2024-07-17 19:04:51 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,29 +1,25 @@
-    });
-function generateRandomData() {
-
-main();
-
-}
-    }
-    return data;
-function main() {
-    data.forEach(item => {
-}
-
-    const data = generateRandomData();
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
-    const data = [];
-        console.log("Random Number: " + item);public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
             data[i] = (int) (Math.random() * 100) + 1;
         }
-        for (int i = 0; i < 10; i++) {
 
+public class RandomDataGenerator {
 }
-
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
     public static void main(String[] args) {
         }
+            System.out.println("Random Number: " + item);
     }
+
         for (int item : data) {
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
+
+        int[] data = new int[10];
+}
+    public static void main(String[] args) {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
